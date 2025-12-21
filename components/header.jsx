d5355830c8fca34bae7c03d0abfb1b6551f0256a
Braj Kishor Singh <brajkishorsingh@Brajs-MacBook-Air.local>
2025-12-22 02:00:58 +0530

    authentication added for covex

--- components/header.jsx
+++ components/header.jsx
@@ -1,20 +1,25 @@
-import { SignInButton, SignUpButton, UserButton,SignedOut,SignedIn } from "@clerk/nextjs";
+"use client"
+
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
 
 const Header = () => {
     return (
-        <div className="fixed top-6 left-1/2 trasform -translate-x-1/2 z-50 w-full max-3-3xl px-4">
-            <SignedOut>
+        <header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-3xl px-4">
+              <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-4 sm:px-6 md:px-8 py-3 flex items-center justify-between gap-2">
+            <Unauthenticated>
                 <SignInButton />
                 <SignUpButton>
                     <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                         Sign Up
                     </button>
                 </SignUpButton>
-            </SignedOut>
-            <SignedIn>
+            </Unauthenticated>
+            <Authenticated>
                 <UserButton />
-            </SignedIn>
-        </div>
+            </Authenticated>
+              </div>
+        </header>
     )
 }
 
