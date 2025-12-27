2fd0adc5a7242c19f39db1837887a3294ee1ed37
singhbraj <braj333singh@gmail.com>
2025-12-27 16:46:26 +0530

    created apis for posts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as post from "../post.js";
 import type * as users from "../users.js";
 
 import type {
@@ -17,6 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  post: typeof post;
   users: typeof users;
 }>;
 
